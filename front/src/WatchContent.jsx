81cefe26dc43ee69c767732d31183aac84c70579
semdoce <g.andrade2431@outlook.com>
2026-03-19 15:53:37 -0300

    add react routes

---
+++ front/src/WatchContent.jsx
@@ -0,0 +1,49 @@
+import ReactPlayer from "react-player";
+import {
+  MediaController,
+  MediaControlBar,
+  MediaTimeRange,
+  MediaTimeDisplay,
+  MediaVolumeRange,
+  MediaPlaybackRateButton,
+  MediaPlayButton,
+  MediaSeekBackwardButton,
+  MediaSeekForwardButton,
+  MediaMuteButton,
+  MediaFullscreenButton,
+} from "media-chrome/react";
+import film from '/mnt/HDD/Downloads/film.mp4';
+function WatchContent() {
+  const src = film;
+  return (
+    <MediaController
+      style={{
+        width: "100%",
+        aspectRatio: "16/9",
+      }}
+    >
+      <ReactPlayer
+        slot="media"
+        src={src}
+        controls={false}
+        style={{
+          width: "100%",
+          height: "100%",
+          "--controls": "none",
+        }}
+      ></ReactPlayer>
+      <MediaControlBar>
+        <MediaPlayButton />
+        <MediaSeekBackwardButton seekOffset={10} />
+        <MediaSeekForwardButton seekOffset={10} />
+        <MediaTimeRange />
+        <MediaTimeDisplay showDuration />
+        <MediaMuteButton />
+        <MediaVolumeRange />
+        <MediaPlaybackRateButton />
+        <MediaFullscreenButton />
+      </MediaControlBar>
+    </MediaController>
+  );
+}
+export default WatchContent;
--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './tailwind.css'
 import HomePage from './HomePage.jsx'
+import WatchContent from './WatchContent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HomePage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/video" element={<WatchContent />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
